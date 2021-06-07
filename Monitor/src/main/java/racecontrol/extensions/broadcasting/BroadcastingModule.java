26d77ceefd5ebbea9ac73d3e59a6c54e854e2851
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-06-07 20:36:28 +0200

    Add entry into general config panel to disable the broadcasting panel.

--- Monitor/src/main/java/racecontrol/extensions/broadcasting/BroadcastingModule.java
+++ Monitor/src/main/java/racecontrol/extensions/broadcasting/BroadcastingModule.java
@@ -8,6 +8,7 @@ package racecontrol.extensions.broadcasting;
 import racecontrol.client.AccBroadcastingClient;
 import racecontrol.client.extension.AccBroadcastingClientExtensionModule;
 import racecontrol.client.extension.AccClientExtension;
+import racecontrol.extensions.GeneralExtentionConfigPanel;
 import racecontrol.visualisation.gui.LPContainer;
 
 /**
@@ -19,7 +20,7 @@ public class BroadcastingModule
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return GeneralExtentionConfigPanel.getInstance().isBroadcastingEnabled();
     }
 
     @Override
