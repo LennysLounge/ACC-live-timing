dffb3e598d3c32527aa3dc302939eb6ee7079846
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-06-19 13:34:57 +0200

    Add a panel for debug visuals.

--- Monitor/src/main/java/racecontrol/extensions/base/timing/TimingExtension.java
+++ Monitor/src/main/java/racecontrol/extensions/base/timing/TimingExtension.java
@@ -5,11 +5,15 @@
  */
 package racecontrol.extensions.base.timing;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import racecontrol.client.AccBroadcastingClient;
-import racecontrol.client.data.CarInfo;
+import racecontrol.client.data.RealtimeInfo;
+import racecontrol.client.events.RealtimeCarUpdate;
 import racecontrol.client.events.RealtimeUpdate;
+import racecontrol.client.events.SessionChanged;
 import racecontrol.client.extension.AccClientExtension;
 import racecontrol.eventbus.Event;
 import racecontrol.visualisation.gui.LPContainer;
@@ -21,33 +25,38 @@ import racecontrol.visualisation.gui.LPContainer;
 public class TimingExtension
         extends AccClientExtension {
 
+    private TimingPanel panel;
+
+    private Map<Integer, List<Float>> rawRaceDistance = new HashMap<>();
+
     public TimingExtension(AccBroadcastingClient client) {
         super(client);
+        panel = new TimingPanel(this);
     }
 
     @Override
     public LPContainer getPanel() {
-        return null;
+        return panel;
     }
 
     @Override
     public void onEvent(Event e) {
-        if (e instanceof RealtimeUpdate) {
-
-            try {
-                for (CarInfo car : getClient().getModel().getCarsInfo().values()) {
-                    String fileName = String.valueOf(car.getCarNumber());
-                    FileWriter fw = new FileWriter(fileName, true);
-                    BufferedWriter bw = new BufferedWriter(fw);
-                    bw.write(String.valueOf(car.getRealtime().getSplinePosition()));
-                    bw.write("\t");
-                    bw.write(String.valueOf(car.getRealtime().getLaps()));
-                    bw.newLine();
-                    bw.close();
-                }
-            } catch (Exception ex) {
-
+        if (e instanceof RealtimeCarUpdate) {
+            RealtimeInfo info = ((RealtimeCarUpdate) e).getInfo();
+            if (!rawRaceDistance.containsKey(info.getCarId())) {
+                rawRaceDistance.put(info.getCarId(), new LinkedList<>());
             }
+            float raceDistance = info.getSplinePosition() + info.getLaps();
+            rawRaceDistance.get(info.getCarId()).add(raceDistance);
+        } else if (e instanceof RealtimeUpdate) {
+            int focusedCarId = ((RealtimeUpdate) e).getSessionInfo().getFocusedCarIndex();
+
+            if (rawRaceDistance.containsKey(focusedCarId)) {
+                panel.rawRaceDistance = rawRaceDistance.get(focusedCarId);
+                panel.invalidate();
+            }
+        } else if(e instanceof SessionChanged){
+            rawRaceDistance.clear();
         }
     }
 
---
+++ Monitor/src/main/java/racecontrol/extensions/base/timing/TimingPanel.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2021 Leonard Sch?ngel
+ * 
+ * For licensing information see the included license (LICENSE.txt)
+ */
+package racecontrol.extensions.base.timing;
+
+import java.util.LinkedList;
+import java.util.List;
+import static processing.core.PConstants.OPEN;
+import racecontrol.visualisation.gui.LPContainer;
+
+/**
+ *
+ * @author Leonard
+ */
+public class TimingPanel
+        extends LPContainer {
+
+    private TimingExtension extension;
+
+    public List<Float> rawRaceDistance = new LinkedList<>();
+
+    public TimingPanel(TimingExtension extension) {
+        this.extension = extension;
+    }
+
+    @Override
+    public void draw() {
+        applet.fill(0);
+        applet.rect(20, 20, getWidth() - 40, getHeight() - 40);
+
+        float max = -1;
+        float min = 99999;
+        for (Float v : rawRaceDistance) {
+            if (v > max) {
+                max = v;
+            }
+            if (v < min) {
+                min = v;
+            }
+        }
+
+        applet.stroke(100);
+        for (int i = (int) Math.ceil(min); i < max; i++) {
+            float y = applet.map(i, min, max, 20, getHeight() - 20);
+            applet.line(20, y, getWidth() - 20, y);
+        }
+
+        applet.stroke(255);
+        applet.noFill();
+        applet.beginShape();
+        for (int i = 0; i < rawRaceDistance.size(); i++) {
+            float y = applet.map(rawRaceDistance.get(i), min, max, getHeight()-20, 20);
+            float x = applet.map(i, 0, rawRaceDistance.size()-1, 20, getWidth()-20);
+            applet.vertex(x, y);
+        }
+        applet.endShape(OPEN);
+
+    }
+
+}
